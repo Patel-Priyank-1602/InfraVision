8b8713d0e3a63551da1aa3ba0f48956467fc3133
Patel-Priyank-1602 <patelpriyank2526@gmail.com>
2026-05-25 17:32:23 +0530

    Fixed Again

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,11 +1,1 @@
-import { useSupabaseAuth } from "@/components/SupabaseAuthProvider";
-
-export function useAuth() {
-  const { user, loading } = useSupabaseAuth();
-
-  return {
-    user,
-    isLoading: loading,
-    isAuthenticated: !!user,
-  };
-}
+export {};
